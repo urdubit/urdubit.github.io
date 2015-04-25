f35a361ad36b45f8a9040e76b517955e3044c4b3
Rodrigo <rodrigo@blinktrade.com>
2015-04-25 02:43:57 -0700

    new ui

---
+++ js/ticker.js
@@ -0,0 +1,62 @@
+function estatisticas(){
+		var statisticas ='';
+		$.ajax({
+			url: 'https://api.blinktrade.com/api/v1/PKR/ticker',
+			dataType: 'jsonp',
+			data: {crypto_currency: 'BTC'},
+			async: false
+		})
+		.done(function(statisticas) {
+			$('.last').html("PKR"+statisticas['last']);
+			$('.high').html("PKR"+statisticas['high']);
+			$('.low').html("PKR"+statisticas['low']);
+			$('.vol').html(statisticas['vol']);
+			$('.preco-btc b').html(statisticas['buy'])
+		})
+		.fail(function() {
+			console.log("error");
+		})
+		.always(function() {
+			console.log("complete");
+		});
+		
+
+	}
+
+$(function(){
+	// carrega e atualiza estatisticas
+	estatisticas();
+	setInterval( function(){ estatisticas() }, 4000);	
+
+	// modal do login
+	$('.login-btn').click(function() {
+		$('.login-form').css('display','table').addClass('animated fadeInUp').removeClass('fadeOutDown');
+	});
+
+	$('.login-form .back').click(function(event) {
+		$('.login-form').removeClass('fadeInUp').addClass('fadeOutDown');
+	});
+
+
+	//popover
+	$('[data-toggle="popover"]').popover();
+
+	//tooltip
+	$('[data-toggle="tooltip"]').tooltip();
+
+	// remove video
+	if ( $(window).width() < 780){
+		$('video').remove();
+		$('section').css({
+			background: 'url(img/bg1.jpg) no-repeat center',
+			'background-size': 'cover'
+		});;
+	}
+
+})
+
+//menu
+function showMenu(){
+	$('nav.menu a[hide]').show().addClass('animated fadeInDown');
+	$(this).addClass('ativo')
+}
