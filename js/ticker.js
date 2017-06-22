385e932d7b396bfeb315011cfcf3b69edf432b2d
zaintariqs <zaintariqs@hotmail.com>
2017-06-22 16:58:40 +0500

    Update ticker.js

--- js/ticker.js
+++ js/ticker.js
@@ -1,3 +1,21 @@
+function statisticstwo(){
+	jQuery.ajax({
+            url: "../ticker/tickerdata.php",
+            type: "GET",
+
+            contentType: 'application/json; charset=utf-8',
+            success: function(resultData) {
+                //here is your json.
+                  // process it
+                  console.log(resultData);
+
+            },
+            error : function(jqXHR, textStatus, errorThrown) {
+            },
+
+            timeout: 120000,
+        });
+}
 function estatisticas(){
 		var statisticas ='';
 		$.ajax({
